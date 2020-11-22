f3268ba095e26d6309107726ad312cfb7d32d40c
Raphaël Gertz <git@rapsys.eu>
2020-11-22 02:14:13 +0100

    Prevent answer on empty key

--- www/acme-challenge.php
+++ www/acme-challenge.php
@@ -4,12 +4,12 @@ $conf = '/etc/acme/config';
 
 //Unable to show key.thumbprint couple
 if (
+	//Handle get key parsing
+	empty($_GET['key']) || !preg_match('/^[-_a-zA-Z0-9]+$/', $_GET['key']) ||
 	//Handle config parsing
 	!is_readable($conf) || ($config = file_get_contents($conf)) === false || ($config = json_decode($config)) === null ||
 	//Handle thumbprint file read
-	!is_readable($config->thumbprint) || ($thumbprint = file_get_contents($config->thumbprint)) === false ||
-	//Handle get key parsing
-	empty($_GET['key']) || !preg_match('/^[-_a-zA-Z0-9]+$/', $_GET['key'])
+	!is_readable($config->thumbprint) || empty($thumbprint = file_get_contents($config->thumbprint))
 ) {
 	header((!empty($_SERVER['SERVER_PROTOCOL'])?$_SERVER['SERVER_PROTOCOL']:'HTTP/1.0').' 404 Not Found');
 	exit;
